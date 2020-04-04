8c2a06300c268a80b0f76aba4f6b8271d340a150
Noah Kreiger <noahkreiger@Noahs-MacBook-Pro.local>
2020-04-04 12:19:48 -0400

    initial commit of blackjack api

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import json
+
+# services
+from services.game import BlackjackGame
+
+app = Flask(__name__)
+
+blackJack = BlackjackGame()
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/init_game')
+def init_game():
+    return blackJack.init_game()
+
+
+@app.route('/gameplay/step', methods=['GET', 'POST'])
+def play_hand():
+    if request.method == 'POST':
+        data = request.get_json()
+        return blackJack.step(data['action'])
+    elif request.method == 'GET':
+        return blackJack.get_state(blackJack.get_player_id())
+
+
+if __name__ == '__main__':
+    app.run()
